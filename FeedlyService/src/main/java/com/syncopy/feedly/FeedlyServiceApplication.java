e5ec5ba156c13df042061bcd909d203b71b3f3d6
rajesh.kanta <rajesh.kanta@rajeshkanta.dev@gmail.com>
2017-05-07 21:19:58 -0400

    initial commit for feedly service(Spring framework)

---
+++ FeedlyService/src/main/java/com/syncopy/feedly/FeedlyServiceApplication.java
@@ -0,0 +1,12 @@
+package com.syncopy.feedly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedlyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedlyServiceApplication.class, args);
+	}
+}
